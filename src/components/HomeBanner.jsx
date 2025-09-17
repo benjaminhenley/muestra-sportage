837f8db8967780b06b673c8696b2d56b42eeb0f3
benjahenley <benjahenley@hotmail.com>
2025-09-17 15:56:08 -0300

    FIX - wording

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -21,7 +21,7 @@ export const HomeBanner = () => {
         {/* Contenido - Ahora absolutamente posicionado tanto en móvil como en desktop */}
         <div className="absolute px-1 pb-3 inset-0 flex flex-col items-center justify-end text-white z-20">
           <h1 className="font-bold animate-fade-in">{homeBanner.titulo}</h1>
-          <p className="text-[1rem] py-1 xl:text-[1.5rem] 2xl:text-[2.5rem] 2xl:py-3 win:text-[1rem] win:py-1 mb-6">
+          <p className="text-[1rem] py-1 xl:text-[1.5rem] 2xl:text-[2.5rem] 2xl:py-3 win:text-[1rem] win:py-1 mb-6 ">
             {homeBanner.subtitulo}
           </p>
         </div>
